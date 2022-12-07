51e9b5f27700996fd9bae6610443277497bd5a04
shashikant <surya.indian321@gmail.com>
2022-12-07 18:32:47 +0530

    Node JS First Application Successfully Executedgit add -A

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,4 +5,8 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get('/test', function (req, res) {
+    res.send('My first experiment!')
+});
+
 module.exports = router;
